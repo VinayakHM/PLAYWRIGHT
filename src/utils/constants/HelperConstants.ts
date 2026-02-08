aae9a133839092b3532fce44e4348f4e085937f8
Vinayak M <vinayakhk9@gmail.com>
2026-02-08 23:37:48 +0530

    added Listners and Logger file

---
+++ src/utils/constants/HelperConstants.ts
@@ -0,0 +1,18 @@
+export const constants = {
+  User1: {
+    username: 'test@gmail.com',
+    password: process.env.USER_PWD1 as string,
+  },
+  User2: {
+    username: 'test@gmail.com',
+    password: process.env.USER_PWD2 as string,
+  },
+
+  URLs: {
+    dev: {
+      practice: 'https://practicesoftwaretesting.com/',
+    },
+    stage: {
+      practice: 'https://practicesoftwaretesting.com/',
+    },},
+  };
